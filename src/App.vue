754bf340e1f14efbf40882ec7132ebe132b3e5c0
ArkanDash <suckgamer0@gmail.com>
2025-11-13 22:20:21 +0700

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="text-white">
+    <div class="flex flex-col items-center">
+      <text class="text-6xl">Niya Archive</text>
+
+    </div>
+  </div>
+</template>
