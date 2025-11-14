80b3db0dd86bd4c7b6ba2c7f78b559e338a5e46b
ArkanDash <suckgamer0@gmail.com>
2025-11-14 09:08:39 +0700

    feat: bond & main template

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,53 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import MainStoryContent from './contents/MainStoryContent.vue';
+  import BondStoryContent from './contents/BondStoryContent.vue';
+
+  const currentView = ref('main');
+
+  const setActiveView = (view: string) => {
+    currentView.value = view;
+    console.log('Current View:', currentView.value);
+  };
+</script>
 
 <template>
   <div class="text-white">
-    <div class="flex flex-col items-center">
-      <text class="text-6xl">Niya Archive</text>
+    <div class="flex flex-col items-center bg-gray-800 p-4 rounded-t-md gap-4">
+      <text class="text-6xl font-bold">Niya Archive</text>
+      
+      <div class="flex gap-8 w-full justify-center border-t border-gray-700 pt-4"> 
 
+        <button 
+          @click="setActiveView('main')"
+          :class="{ 
+            'bg-indigo-600 hover:bg-indigo-700 shadow-2xl': currentView !== 'main', 
+            'bg-yellow-500 text-gray-900 shadow-yellow-500/50 scale-105': currentView === 'main' 
+          }"
+          class="p-6 text-xl rounded-xl font-bold transition duration-150 ease-in-out transform"
+        >
+          Main Story
+        </button>
+
+        <button 
+          @click="setActiveView('bond')"
+          :class="{ 
+            'bg-teal-600 hover:bg-teal-700 shadow-2xl': currentView !== 'bond', 
+            'bg-yellow-500 text-gray-900 shadow-yellow-500/50 scale-105': currentView === 'bond' 
+          }"
+          class="p-6 text-xl rounded-xl font-bold transition duration-150 ease-in-out transform"
+        >
+          Bond Story
+        </button>
+      </div>
+    </div>
+    
+    <div class="bg-gray-800 p-4 rounded-b-md">
+      <hr class="border-gray-700 my-4" />
+      
+      <MainStoryContent v-if="currentView === 'main'" />
+      <BondStoryContent v-else />
+      
     </div>
   </div>
 </template>
---
+++ src/contents/BondStoryContent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="text-gray-400 text-center py-8">
+    <h2 class="text-3xl font-bold text-white mb-4">
+      <span class="text-yellow-500">Bond Story</span>
+    </h2>
+    <p class="text-lg text-white">Characters Bond Story</p>
+    <p>List of character bonds.</p>
+    <div class="mt-6 p-4 bg-gray-700 inline-block rounded-lg">
+      <p class="text-pink-400">To be implemented...</p>
+    </div>
+  </div>
+</template>
